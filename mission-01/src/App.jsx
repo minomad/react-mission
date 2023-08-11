f8e05ab048aef687d5909f181f140272409785ef
minomad <93kymins@gmail.com>
2023-08-11 12:19:17 +0900

    버튼 정렬 수정 flex-col

--- mission-01/src/App.jsx
+++ mission-01/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="flex justify-center py-10 gap-4">
+    <div className="flex flex-col items-center py-10 gap-4">
       <Button color="primary">로그인</Button>
       <Button color="secondary">회원가입</Button>
       <Button color="disabled">Disabled</Button>
