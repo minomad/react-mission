af2782fbc83ea9141e23785a54f4591d77a47631
minomad <93kymins@gmail.com>
2023-08-20 11:33:00 +0900

    NewsPage - Spinner 추가

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -1,3 +1,5 @@
+import Error from '@/components/Error';
+import Spinner from '@/components/Spinner';
 import NewsList from '@/components/NewsList';
 import useFetchData from '@/hook/useFetchData';
 import NewsCategory from '@/components/NewsCategory';
@@ -6,8 +8,16 @@ import useDocumentTitle from '@/hook/useDocumentTitle';
 function NewsPage() {
   useDocumentTitle('뉴스 페이지');
 
-  const { news, selectedCategory, setSelectedCategory } = useFetchData('');
+  const { news, state, selectedCategory, setSelectedCategory } = useFetchData('');
 
+  if (state === 'loading') {
+    return <Spinner />;
+  }
+
+  if (state === 'error') {
+    return <Error />;
+  }
+  
   return (
     <>
       <NewsCategory selectedCategory={selectedCategory} handleChangeCategory={setSelectedCategory} />
